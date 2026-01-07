a8ffbcb791ea1bbdac08b34fd564367ceff087eb
Kevin Schern <kevins@illumenate.lighting>
2026-01-07 13:47:05 -0800

    Add Illumenate doctype definitions

---
+++ illumenate_lighting/illumenate_lighting/doctype/ill_attribute_sdcm/test_ill_attribute_sdcm.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, ilLumenate Lighting and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestilLAttributeSDCM(FrappeTestCase):
+	pass
