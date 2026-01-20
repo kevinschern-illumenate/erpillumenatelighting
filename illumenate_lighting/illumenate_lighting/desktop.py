d15f9b8a2385e3904dd0b43c9088bd0529546703
kevinscherncascade <KEVINS@ILLUMENATE.LIGHTING>
2026-01-20 08:32:00 -0800

    Create desktop.py

---
+++ illumenate_lighting/illumenate_lighting/desktop.py
@@ -0,0 +1,65 @@
+def get_data():
+    return [
+        {
+            "module_name": "ilLumenate Lighting",
+            "color": "#6c757d",
+            "icon": "octicon octicon-flame",
+            "label": "ilLumenate Lighting",
+            "items": [
+                {"type": "doctype", "name": "ilL-Attribute-CCT"},
+                {"type": "doctype", "name": "ilL-Attribute-CRI"},
+                {"type": "doctype", "name": "ilL-Attribute-Dimming Protocol"},
+                {"type": "doctype", "name": "ilL-Attribute-Endcap Color"},
+                {"type": "doctype", "name": "ilL-Attribute-Endcap Style"},
+                {"type": "doctype", "name": "ilL-Attribute-Environment Rating"},
+                {"type": "doctype", "name": "ilL-Attribute-Finish"},
+                {"type": "doctype", "name": "ilL-Attribute-Joiner Angle"},
+                {"type": "doctype", "name": "ilL-Attribute-Joiner System"},
+                {"type": "doctype", "name": "ilL-Attribute-Lens Appearance"},
+                {"type": "doctype", "name": "ilL-Attribute-Lens Interface Type"},
+                {"type": "doctype", "name": "ilL-Attribute-LED Package"},
+                {"type": "doctype", "name": "ilL-Attribute-Lead Time Class"},
+                {"type": "doctype", "name": "ilL-Attribute-Output Level"},
+                {"type": "doctype", "name": "ilL-Attribute-Output Voltage"},
+                {"type": "doctype", "name": "ilL-Attribute-Power Feed Type"},
+                {"type": "doctype", "name": "ilL-Attribute-Pricing Class"},
+                {"type": "doctype", "name": "ilL-Attribute-SDCM"},
+
+                {"type": "doctype", "name": "ilL-Fixture-Template"},
+                {"type": "doctype", "name": "ilL-Spec-Profile"},
+                {"type": "doctype", "name": "ilL-Spec-Lens"},
+                {"type": "doctype", "name": "ilL-Spec-LED Tape"},
+                {"type": "doctype", "name": "ilL-Spec-Driver"},
+                {"type": "doctype", "name": "ilL-Spec-Accessory"},
+                {"type": "doctype", "name": "ilL-Configured-Fixture"},
+
+                {"type": "doctype", "name": "ilL-Project"},
+                {"type": "doctype", "name": "ilL-Project-Fixture-Schedule"},
+
+                {"type": "doctype", "name": "ilL-Document-Request"},
+                {"type": "doctype", "name": "ilL-Request-Type"},
+                {"type": "doctype", "name": "ilL-Request-Type-Field"},
+                {"type": "doctype", "name": "ilL-Request-Deliverable"},
+                {"type": "doctype", "name": "ilL-Request-Field-Value"},
+
+                {"type": "doctype", "name": "ilL-Export-Job"},
+
+                {"type": "doctype", "name": "ilL-Rel-Tape Offering"},
+                {"type": "doctype", "name": "ilL-Rel-Leader-Cable-Map"},
+                {"type": "doctype", "name": "ilL-Rel-Mounting-Accessory-Map"},
+                {"type": "doctype", "name": "ilL-Rel-Driver-Eligibility"},
+                {"type": "doctype", "name": "ilL-Rel-Endcap-Map"},
+
+                {"type": "doctype", "name": "ilL-Child-Template-Allowed-Option"},
+                {"type": "doctype", "name": "ilL-Child-Template-Allowed-TapeOffering"},
+                {"type": "doctype", "name": "ilL-Child-User-Segment"},
+                {"type": "doctype", "name": "ilL-Child-Schedule-Collaborator"},
+                {"type": "doctype", "name": "ilL-Child-Project-Collaborator"},
+                {"type": "doctype", "name": "ilL-Child-Profile-Environment"},
+                {"type": "doctype", "name": "ilL-Child-Pricing-Snapshot"},
+                {"type": "doctype", "name": "ilL-Child-Fixture-Schedule-Line"},
+                {"type": "doctype", "name": "ilL-Child-Configured-Run"},
+                {"type": "doctype", "name": "ilL-Child-Configured-Segment"}
+            ],
+        }
+    ]
