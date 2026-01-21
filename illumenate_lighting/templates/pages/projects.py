f0b1ea2b4da304919b2d4dd108a7ceae19bad616
kevinscherncascade <KEVINS@ILLUMENATE.LIGHTING>
2026-01-21 11:02:50 -0800

    fix

--- illumenate_lighting/templates/pages/projects.py
+++ illumenate_lighting/templates/pages/projects.py
@@ -9,7 +9,19 @@ no_cache = 1
 def get_context(context):
 	"""Get context for the projects list portal page."""
 	if frappe.session.user == "Guest":
-		frappe.throw("Please login to view projects", frappe.PermissionError)
+		frappe.local.flags.redirect_location = "/login"
+		raise frappe.Redirect
+
+	# Check if user has any role that grants access to ilL-Project
+	user_roles = set(frappe.get_roles(frappe.session.user))
+	allowed_roles = {"System Manager", "Administrator", "Dealer", "Customer"}
+
+	if not user_roles & allowed_roles:
+		frappe.throw(
+			"You don't have permission to access projects. "
+			"Please contact your administrator.",
+			frappe.PermissionError
+		)
 
 	# Get projects accessible to this user using permission hooks
 	projects = frappe.get_all(
