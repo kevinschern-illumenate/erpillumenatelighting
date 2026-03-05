b10d22fd306d5a2d801c2c4dea33b3485f86f4f5
kevinscherncascade <KEVINS@ILLUMENATE.LIGHTING>
2026-03-05 10:31:55 -0800

    fix

--- illumenate_lighting/illumenate_lighting/doctype/ill_rel_mounting_accessory_map/ill_rel_mounting_accessory_map.py
+++ illumenate_lighting/illumenate_lighting/doctype/ill_rel_mounting_accessory_map/ill_rel_mounting_accessory_map.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2026, ilLumenate Lighting and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+
+VALID_TEMPLATE_TYPES = ("ilL-Fixture-Template", "ilL-Tape-Neon-Template")
 
 
 class ilLRelMountingAccessoryMap(Document):
-	pass
+	def before_save(self):
+		if not self.template_type:
+			self.template_type = "ilL-Fixture-Template"
+		if self.template_type not in VALID_TEMPLATE_TYPES:
+			frappe.throw(f"Invalid template type '{self.template_type}'. Must be one of {VALID_TEMPLATE_TYPES}")
