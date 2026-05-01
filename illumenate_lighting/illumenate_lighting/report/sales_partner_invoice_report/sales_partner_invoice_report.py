8ed53c8f168f1ddb7b9410b77a320b520c73af2a
kevinscherncascade <KEVINS@ILLUMENATE.LIGHTING>
2026-05-01 10:44:25 -0700

    asdf

---
+++ illumenate_lighting/illumenate_lighting/report/sales_partner_invoice_report/sales_partner_invoice_report.py
@@ -0,0 +1,97 @@
+import frappe
+from frappe import _
+
+
+def execute(filters=None):
+    filters = filters or {}
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+
+def get_columns():
+    return [
+        {
+            "label": _("Invoice ID"),
+            "fieldname": "name",
+            "fieldtype": "Link",
+            "options": "Sales Invoice",
+            "width": 180,
+        },
+        {
+            "label": _("Sales Partner"),
+            "fieldname": "sales_partner",
+            "fieldtype": "Link",
+            "options": "Sales Partner",
+            "width": 200,
+        },
+        {
+            "label": _("Net Total"),
+            "fieldname": "net_total",
+            "fieldtype": "Currency",
+            "width": 140,
+        },
+        {
+            "label": _("Grand Total"),
+            "fieldname": "grand_total",
+            "fieldtype": "Currency",
+            "width": 140,
+        },
+        {
+            "label": _("Posting Date"),
+            "fieldname": "posting_date",
+            "fieldtype": "Date",
+            "width": 120,
+        },
+        {
+            "label": _("Payment Due Date"),
+            "fieldname": "due_date",
+            "fieldtype": "Date",
+            "width": 140,
+        },
+    ]
+
+
+def get_data(filters):
+    conditions = []
+    values = {}
+
+    if filters.get("from_date"):
+        conditions.append("posting_date >= %(from_date)s")
+        values["from_date"] = filters["from_date"]
+
+    if filters.get("to_date"):
+        conditions.append("posting_date <= %(to_date)s")
+        values["to_date"] = filters["to_date"]
+
+    if filters.get("sales_partner"):
+        conditions.append("sales_partner = %(sales_partner)s")
+        values["sales_partner"] = filters["sales_partner"]
+
+    if filters.get("company"):
+        conditions.append("company = %(company)s")
+        values["company"] = filters["company"]
+
+    where_clause = "docstatus = 1"
+    if conditions:
+        where_clause += " AND " + " AND ".join(conditions)
+
+    return frappe.db.sql(
+        """
+        SELECT
+            name,
+            sales_partner,
+            net_total,
+            grand_total,
+            posting_date,
+            due_date
+        FROM
+            `tabSales Invoice`
+        WHERE
+            {where_clause}
+        ORDER BY
+            posting_date DESC, name DESC
+        """.format(where_clause=where_clause),
+        values,
+        as_dict=True,
+    )
